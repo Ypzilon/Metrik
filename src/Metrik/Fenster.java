95e4b35c62a7330cabeec9efdfe9648356179c0a
Ypzilon <schuelerst@yahoo.de>
2020-10-27 18:43:34 +0100

    Upload Version 1.0

---
+++ src/Metrik/Fenster.java
@@ -0,0 +1,47 @@
+package Metrik;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class Fenster extends Frame implements WindowListener{
+	static int kreisCount;
+	private static Zeichner fZeichner;
+	
+	public Fenster() {
+		kreisCount = 0;
+		fZeichner = new Zeichner();
+		
+		setTitle("Metrik 1.0");
+		setSize(500, 500);
+		setVisible(true);
+		
+		addWindowListener(this);
+		add(fZeichner);
+	}
+	
+	public static int getKreisCount() {
+		return kreisCount;
+	}
+	
+	public static void setKreisCount(int k) {
+		kreisCount = k;
+	}
+
+	public void windowActivated(WindowEvent arg0) {}
+
+	public void windowClosed(WindowEvent arg0) {}
+	
+	public void windowClosing(WindowEvent arg0) {
+		dispose();
+		System.exit(0);
+	}
+
+	public void windowDeactivated(WindowEvent arg0) {}
+
+	public void windowDeiconified(WindowEvent arg0) {}
+
+	public void windowIconified(WindowEvent arg0) {}
+
+	public void windowOpened(WindowEvent arg0) {}
+
+}
---
+++ src/Metrik/Zeichner.java
@@ -0,0 +1,61 @@
+package Metrik;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class Zeichner extends Canvas implements MouseListener{
+	private int durchmesser;
+	private int xKoordinate, yKoordinate;
+	
+	public Zeichner() {
+		durchmesser = 50;
+		setSize(500, 500);
+		addMouseListener(this);
+	}
+
+	public void paint(Graphics2D g2d) {
+		if(Fenster.getKreisCount() == 2) {
+			g2d.clearRect(0, 0, 500, 500);
+			Fenster.setKreisCount(0);
+		}else {
+			g2d.drawOval(xKoordinate, yKoordinate, durchmesser, durchmesser);
+			Fenster.setKreisCount(Fenster.getKreisCount() +1);
+		}
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent event) {
+		Graphics2D g2d = (Graphics2D) getGraphics();
+		
+		if(event.getClickCount() == 2) {
+			xKoordinate = event.getX() - durchmesser/2;
+			yKoordinate = event.getY() - durchmesser/2;
+			paint(g2d);
+		}
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
