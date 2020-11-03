d8a5d848bb438f5dc0c90472f3de780e382966cb
Ypzilon <schuelerst@yahoo.de>
2020-11-03 09:33:04 +0100

    Upload Version 1.2.1

--- src/Metrik/Zeichner.java
+++ src/Metrik/Zeichner.java
@@ -6,6 +6,7 @@ import java.awt.event.*;
 public class Zeichner extends Canvas implements MouseListener{
 	private int durchmesser;
 	private int xKoordinate, yKoordinate;
+	private double xRoot=0, yRoot=0;
 	
 	public Zeichner() {
 		durchmesser = 50;
@@ -16,18 +17,40 @@ public class Zeichner extends Canvas implements MouseListener{
 	public void paint(Graphics2D g2d) {
 		g2d.setFont(new Font("Consolas", Font.PLAIN, 16));
 		
-		if(Fenster.getKreisCount() == 2) {
-			g2d.clearRect(0, 0, 500, 500);
-			Fenster.setKreisCount(0);
-		}else {
-			Fenster.setKreisCount(Fenster.getKreisCount() +1);
+		if(Fenster.getKreisCount() == 0) {
+			Fenster.setKreisCount(1);
 			g2d.drawOval(xKoordinate, yKoordinate, durchmesser, durchmesser);
 			g2d.drawString("Kreis "+Fenster.getKreisCount()+": X:"+xKoordinate+" Y:"+yKoordinate, 20, (20*Fenster.getKreisCount()));
+			xRoot = (double) xKoordinate;
+			yRoot = (double) yKoordinate;
+		}else if(Fenster.getKreisCount() == 1) {
+			Fenster.setKreisCount(2);
+			g2d.drawOval(xKoordinate, yKoordinate, durchmesser, durchmesser);
+			g2d.drawString("Kreis "+Fenster.getKreisCount()+": X:"+xKoordinate+" Y:"+yKoordinate, 20, (20*Fenster.getKreisCount()));
+			xRoot -= (double) xKoordinate;
+			yRoot -= (double) yKoordinate;
+			g2d.drawString("Distanz: "+getDistance(), 20, (20+20*Fenster.getKreisCount()));
+		}else {
+			g2d.clearRect(0, 0, 500, 500);
+			Fenster.setKreisCount(0);
+			reset();
 		}
+		
 	}
 	
 	public void setDurchmesser(int d) {
 		durchmesser = d;
+	}
+	
+	public double getDistance() {
+		return Math.sqrt(Math.pow(xRoot, 2)+Math.pow(yRoot, 2))-durchmesser;
+	}
+	
+	public void reset() {
+		xKoordinate = 0;
+		yKoordinate = 0;
+		xRoot = 0;
+		yRoot = 0;
 	}
 	
 	@Override
